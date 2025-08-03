09cfa3648b027fe39be9f10deeb614dca347f417
L3tzG0 <ggletzgo@gmail.com>
2025-08-03 22:32:33 +0700

    fixed tailwind

---
+++ userside/frontend/postcss.config.js
@@ -0,0 +1,20 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// }
