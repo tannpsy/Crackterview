48f5bc778b8a1ee97b251c7b7f5a7d684f11bbd2
L3tzG0 <ggletzgo@gmail.com>
2025-08-07 10:17:48 +0700

    profile fix

--- src/usersidefrontend/components/cards/ProfileInfoCard.jsx
+++ src/usersidefrontend/components/cards/ProfileInfoCard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
+import { getInitials } from "../../utils/helper";
 
 const ProfileInfoCard = () => {
     const { user, loading, clearUser } = useContext(UserContext);
@@ -22,11 +23,17 @@ const [localUser, setLocalUser] = useState(user);
     return (
         user && (
         <div className='flex items-center'>
-            <img
+            {user.profileImageUrl ? (
+                <img
                 src={user.profileImageUrl}
                 alt=''
                 className='w-11 h-11 bg-gray-300 rounded-full mr-3'
-            />
+                />
+            ) : (
+                <div className='w-11 h-11 bg-gray-300 rounded-full mr-3 flex items-center justify-center text-xl font-bold text-white'>
+                {getInitials(user.username || "")}
+                </div>
+            )}
             <div>
                 <div
                     className='text-[15px] text-black font-bold leading-3'
